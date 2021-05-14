d8b69585d1c1a59fd1ba1ae50a845302e09fd8da
lijunjie <lijunjie@windimg.com>
2021-05-14 11:40:10 +0800

    增加一些测试demo

--- app/src/main/java/com/bn/pd/javassist/PersonService.java
+++ app/src/main/java/com/bn/pd/javassist/PersonService.java
@@ -1,11 +1,14 @@
 package com.bn.pd.javassist;
 
+import android.util.Log;
+
 public class PersonService {
 
+    public static final String TAG = "PersonService";
     int test1 = 0;
-    int test2 = 1;
-    String testStr1 = "test1";
-    String testStr2 = "test2";
+    String testStr1 = "testStr";
+    static int test2 = 1;
+    static String testStr2 = "staticStr2";
     public void getPerson(){
         System.out.println("get Person");
     }
@@ -14,8 +17,22 @@ public class PersonService {
         System.out.println("oh my god,I can fly");
     }
 
+    public static void testStaticMethod(int paramInt,String paramStr) {
+        Log.e(TAG,"testStaticMethod,paramInt="+paramInt+",paramStr:"+paramStr);
+    }
+
+    public static String testStaticMethodToStr(int paramInt,String paramStr) {
+        Log.e(TAG,"testStaticMethod,paramInt="+paramInt+",paramStr:"+paramStr);
+        return paramStr+paramInt;
+    }
+
+    public void testMethod(int paramInt,String paramStr) {
+        Log.e(TAG,"testStaticMethod,paramInt="+paramInt+",paramStr:"+paramStr);
+    }
+
     public String toastText() {
         return "This is toast text";
-
     }
+
+
 }
