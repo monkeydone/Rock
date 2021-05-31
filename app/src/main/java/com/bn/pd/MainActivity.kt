5ccb456b2c355edce512ae3cc35d623d02dd81f7
lijunjie <lijunjie@windimg.com>
2021-05-31 21:45:52 +0800

    移植自动查找fragment能力

--- app/src/main/java/com/bn/pd/MainActivity.kt
+++ app/src/main/java/com/bn/pd/MainActivity.kt
@@ -3,10 +3,9 @@ package com.bn.pd
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.a.findfragment.ListActivity
 import com.bn.pd.databinding.ActivityMainBinding
-import com.bn.pd.javassist.PersonService
 import com.bn.utils.ContextUtils
-import com.bn.utils.toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,8 +15,9 @@ class MainActivity : AppCompatActivity() {
         ContextUtils.init(baseContext.applicationContext)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.tvHello.setOnClickListener {
+            ListActivity.startListActivity(this)
 
-            PersonService().testStr1.toast()
+//            PersonService().testStr1.toast()
 //            Toast.makeText(it.getContext(), PersonService().testStr1, Toast.LENGTH_LONG).show()
         }
         val content = """
