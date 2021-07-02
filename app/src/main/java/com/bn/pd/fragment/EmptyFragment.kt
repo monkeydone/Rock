7409dcc3d295dedb30c218dfde23845d18cc9f8a
lijunjie <lijunjie@windimg.com>
2021-07-02 17:11:41 +0800

    调整拉取的版本号

--- app/src/main/java/com/bn/pd/fragment/EmptyFragment.kt
+++ app/src/main/java/com/bn/pd/fragment/EmptyFragment.kt
@@ -8,9 +8,10 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.a.findfragment.EmptyFragment
+import com.a.processor.ListFragmentAnnotation
 import java.util.*
 
-//@ListFragmentAnnotation("简单例子2", "root")
+@ListFragmentAnnotation("简单例子2", "root")
 class TestFragment : Fragment() {
 
     override fun onCreateView(
