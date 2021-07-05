70a28f3be61184c05de7a414c74bdc7bfb0810b4
lijunjie <lijunjie@windimg.com>
2021-07-05 20:37:36 +0800

    调整UI

--- app/src/main/java/com/bn/pd/fragment/ExpandableFragment.kt
+++ app/src/main/java/com/bn/pd/fragment/ExpandableFragment.kt
@@ -1,34 +1,55 @@
 package com.bn.pd.fragment
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.a.findfragment.EmptyFragment
 import com.a.findfragment.FragmentAnnotation
 import com.bn.pd.R
-import java.util.*
+import com.bn.pd.databinding.ExpandableFragmentBinding
 
 @FragmentAnnotation("扩展的TextView", "UI")
 class ExpandableFragment : Fragment() {
+    lateinit var binding: ExpandableFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.expandable_fragment, container, false)
+
+        binding = DataBindingUtil.inflate(this.layoutInflater, R.layout.expandable_fragment, null, false)
+
+//        val view = inflater.inflate(R.layout.expandable_fragment, container, false)
 //        val view = FrameLayout(container!!.context)
-        view.setBackgroundColor(getRandomColor())
-        return view
+//        view.setBackgroundColor(getRandomColor())
+
+        val content = """
+                   binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvHello.setOnClickListener {
+
+            PersonService().testStr1.toast()
+//            Toast.makeText(it.getContext(), PersonService().testStr1, Toast.LENGTH_LONG).show()
+        }        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvHello.setOnClickListener {
+
+            PersonService().testStr1.toast()
+//            Toast.makeText(it.getContext(), PersonService().testStr1, Toast.LENGTH_LONG).show()
+        }
+
+        """.trimIndent()
+
+        binding.tvExpandable.addMessage("点我", content)
+        binding.tvExpandable.addMessage("另外一个title", content)
+
+//        binding.addMessage("说明", content)
+//        binding.tvExpandable.addMessage("说明", content)
+        return binding.root
     }
 
-    private fun getRandomColor(): Int {
-        val random = Random()
-        return Color.rgb(random.nextInt(254), random.nextInt(254), random.nextInt(254))
-    }
 
     companion object {
         fun newInstance(): Fragment = EmptyFragment()
