69d234537eb437a5adbad9a6b67ee6b870c098d5
lijunjie <lijunjie@windimg.com>
2021-08-11 17:31:08 +0800

    晚上正则表达式

--- app/src/main/java/com/bn/pd/mvvm/fragment/RegexFragment.kt
+++ app/src/main/java/com/bn/pd/mvvm/fragment/RegexFragment.kt
@@ -76,6 +76,95 @@ class RegexFragment : RBaseFragment<RegexViewModel, FragmentRegexBinding>(), Vie
         return sb
     }
 
+    private fun match1() {
+        val r1 = Regex("[a-z]+")
+        val r2 = Regex("[a-z]+", RegexOption.IGNORE_CASE)
+        val text = "ABCdef"
+        val content = """
+            regex: ${r1.pattern}
+            text: ${text} 
+            result:${r1.matches(text)}
+            
+            regex: ${r2.pattern} ignore case
+            text:${text}
+            result:${r2.matches(text)}
+        """.trimIndent()
+        binding.tvMatch.text = content
+    }
+
+    private fun match2() {
+        val r1 = Regex("[a-z]+")
+        val r2 = Regex("[a-z]+")
+        val text = "ABCdef"
+        val text2 = "ABC"
+        val content = """
+            regex: ${r1.pattern}
+            text: ${text} 
+            result:${r1.containsMatchIn(text)}
+            
+            regex: ${r2.pattern}
+            text:${text2}
+            result:${r2.containsMatchIn(text2)}
+        """.trimIndent()
+        binding.tvMatchContains.text = content
+    }
+
+
+    private fun replace1() {
+        val r1 = Regex("[a-z]+")
+        val r2 = Regex("[0-9]+")
+        val r3 = Regex("""\[\^[0-9]+\]""")
+        val text = "ABCdef"
+        val text2 = "1234ABC1234"
+        val text3 = """
+            [^1]
+            this is text
+            [^2]
+            this is text2
+        """.trimIndent()
+        val content = """
+            regex: ${r1.pattern}
+            text: ${text} 
+            replace: result to 1234
+            result:${r1.replace(text, "1234")}
+            
+            regex: ${r2.pattern}
+            text:${text2}
+            replace: result * 2
+            result:${r2.replace(text2, { (it.value.toInt() * 2).toString() })}
+            
+            regex: ${r3.pattern}
+            text: ${text3}
+            replace: ^ to ""
+            result:${r3.replace(text3, { it.value.toString().replace("^", "") })}
+            
+        """.trimIndent()
+        binding.tvReplace.text = content
+    }
+
+
+    private fun matchEntire() {
+        val r1 = Regex("[0-9]+")
+        val text = "ABCdef"
+        val text2 = "12341234"
+        val content = """
+            regex: ${r1.pattern}
+            text: ${text} 
+            result:${r1.matchEntire(text)}
+            
+            regex: ${r1.pattern}
+            text:${text2}
+            result:${r1.matchEntire(text2)}
+            
+            regex: ${r1.pattern}
+            text: ${text2}
+            result:${r1.matchEntire(text2)?.value}
+            
+        """.trimIndent()
+        binding.tvMatchEntire.text = content
+    }
+
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.tv_findall -> {
@@ -93,6 +182,18 @@ class RegexFragment : RBaseFragment<RegexViewModel, FragmentRegexBinding>(), Vie
             R.id.tv_find_replace_all -> {
                 val result = findAll4()
                 binding.tvFindReplaceAll.text = result
+            }
+            R.id.tv_match -> {
+                match1()
+            }
+            R.id.tv_match_contains -> {
+                match2()
+            }
+            R.id.tv_replace -> {
+                replace1()
+            }
+            R.id.tv_match_entire -> {
+                matchEntire()
             }
         }
     }
